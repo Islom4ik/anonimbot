a1b34c2f4cd9789b1b8cf81c090d1360aaff992b
Muhammad <muxammadalixanov9@gmail.com>
2022-11-02 23:24:58 +0500

    new

--- index.js
+++ index.js
@@ -28,42 +28,59 @@ let admquiz;
 let anontrueid;
 bot.on("message", async ctx => {
     try {
-        if(ctx.chat.id == '933981477') {
+        if(ctx.chat.id == '5103314362') {
+            /* '933981477' */
             let anonid = await collection.findOne({anonId: ctx.message.text});
             if(anonid == null) {
                 await ctx.reply(`Я не нашел в базе данное ID: ${ctx.message.text}`)
             }else {
                 admquiz = await ctx.replyWithHTML(`По id: <i>${ctx.message.text}</i> найдено:\n${anonid.user_anonmsg}`, Markup.inlineKeyboard(
                     [
-                        [Markup.button.callback('Одобрить', 'acc'), Markup.button.callback('Отказать', 'unacc')]
+                        [Markup.button.callback('Одобрить', 'acc'), Markup.button.callback('Отказать', 'unacc')],
+                        [Markup.button.callback('Данные Анонима', 'watch')]
                     ]
                 ))
                 anontrueid = await collection.findOne({anonId: ctx.message.text});
             }
         }else {
-            if (ctx.session?.booksRequest> (new Date().valueOf() - 5 * 60 * 1000)) return await ctx.reply(`⚠ Не так быстро! Повторно отправить сообщение можно через 10 мин`);
-            ctx.session.booksRequest = new Date().valueOf(); 
-            let count = await collection.findOne({_id: ObjectId('63612b27b24e538f644ad357')})
-            let result = await count.anonim_message_count + 1;
-            let quat = await ctx.replyWithHTML(`Вы уверены что хотите отправить анонимное сообщение:\n<i>${ctx.message.text}</i>`, Markup.inlineKeyboard(
-                [
-                    [Markup.button.callback('Да, уверен', 'yes'), Markup.button.callback('Нет, отменить', 'no')]
-                ]
-            ))
-            await collection.insertOne(
-                {
-                    user_id: ctx.message.from.id,
-                    user_anonmsg: ctx.message.text,
-                    quiz_markup: `${quat.message_id}`,
-                    anonId: `#${result}`
-                }
-            )   
-            await collection.findOneAndUpdate({_id: ObjectId('63612b27b24e538f644ad357')}, {$set: {anonim_message_count: result}});
+            let request = await collection.findOne({user_id: ctx.message.from.id});
+            if(request == null) {
+                let count = await collection.findOne({_id: ObjectId('63612b27b24e538f644ad357')})
+                let result = await count.anonim_message_count + 1;
+                let quat = await ctx.replyWithHTML(`Вы уверены что хотите отправить анонимное сообщение:\n<i>${ctx.message.text}</i>`, Markup.inlineKeyboard(
+                    [
+                        [Markup.button.callback('Да, уверен', 'yes'), Markup.button.callback('Нет, отменить', 'no')]
+                    ]
+                ))
+                await collection.insertOne(
+                    {
+                        user_id: ctx.message.from.id,
+                        user_name: ctx.message.from.username,
+                        user_firstname: ctx.message.from.first_name,
+                        user_anonmsg: ctx.message.text,
+                        quiz_markup: `${quat.message_id}`,
+                        anonId: `#${result}`
+                    }
+                )   
+                await collection.findOneAndUpdate({_id: ObjectId('63612b27b24e538f644ad357')}, {$set: {anonim_message_count: result}});
+            }else {
+                await ctx.reply('Ожидайте в нашем канале, вы уже отправляли свое сообщение!')
+            }
         }
     }catch(e) {
         console.error(e);
     }   
     // -1001514376747
+})
+
+
+bot.action("watch", async ctx => {
+    try {
+        await ctx.replyWithHTML(`Данные Анонима:\n\nFirst Name: <a href="tg://user?id=${anontrueid.user_id}">${anontrueid.user_firstname}</a>\nUser Name: @${anontrueid.user_name}`)
+        await ctx.answerCbQuery('Не забудьте очистить следы..');
+    }catch(e){
+        console.error(e);
+    }  
 })
 
 bot.action('acc', async ctx => {
@@ -90,14 +107,14 @@ bot.action('unacc', async ctx => {
         console.error(e);
     }
 })
-
+  
 
 
 
 bot.action('yes', async ctx => {
     try {
         let anonserch = await collection.findOne({user_id: ctx.callbackQuery.from.id});
-        await ctx.tg.sendMessage(933981477, `Новое анонимное сообщение ${anonserch.anonId}\n\nДля проверки анонимного сообщения напишите ID сообщения(#00000)`);
+        await ctx.tg.sendMessage(5103314362, `Новое анонимное сообщение ${anonserch.anonId}\n\nДля проверки анонимного сообщения напишите ID сообщения(#00000)`);
         await ctx.tg.deleteMessage(ctx.chat.id, anonserch.quiz_markup);
         await ctx.answerCbQuery('Ваше сообщение отправлено на проверку, ожидайте...')
     }catch(e){
